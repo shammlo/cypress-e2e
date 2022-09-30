8b9d6b8fd53ec36a064139e46c24fd9a5f83d648
shammlo <shamlooo@yahoo.com>
2022-09-30 23:18:09 +0300

    Example one

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,5 +1,33 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://twitter.com/");
+before(() => {});
+
+const uploadFile = () => {
+  return cy.fixture("../fixtures/logo.png").then((image) => {
+    const blob = Cypress.Blob.base64StringToBlob(image, "image/png");
+    const file = new File([blob], "../fixtures/logo.png", {
+      type: "image/png",
+    });
+    return file;
+  });
+};
+
+const attachment = async () => {
+  // return new Promise((resolve) => {
+  //   return resolve(uploadFile());
+  // });
+
+  let bidderJson = {};
+  await new Promise((resolve) => {
+    uploadFile().then(async (res) => {
+      bidderJson = { ...res };
+
+      resolve(bidderJson);
+    });
+  });
+  return bidderJson;
+};
+
+describe("Test", () => {
+  it("Test", () => {
+    console.log(attachment());
   });
 });
